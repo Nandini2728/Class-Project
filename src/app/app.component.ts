ed6e9e106801c4ff30b97eef0060a9c10647b573
Goutham Chalamasetty <chalamasetty.k@gmail.com>
2021-04-12 17:10:00 -0500

    new code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { ProfileResultsMock } from './profile-results.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-project';
+  title = 'Student Profile';
+  studentForm = new FormGroup({})
+  profileResults:any = ProfileResultsMock;
+  paginationConfig = {
+    itemsPerPage: 5,
+    currentPage: 1,
+    totalItems: this.profileResults.count
+  };
+  finalDisplayResults: any;
+
+  constructor() {
+    this.studentForm = new FormGroup({
+      firstname: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required),
+      department: new FormControl('', Validators.required),
+      country: new FormControl('', Validators.required),
+      createdDate: new FormControl()
+    });
+    this.paginationIdsCalculation();
+  }
+
+  paginationIdsCalculation() {
+    this.profileResults.forEach((element,index) => {
+      element.id = index + 1;
+    });
+    this.finalDisplayResults = JSON.parse(JSON.stringify(this.profileResults));
+  }
+
+  onStudentFormSubmit() {
+    if(this.studentForm.valid) {
+      this.studentForm.value.createdDate = Date.now();
+      console.log(this.studentForm.value);
+    }
+  }
+  pageChanged(event) {
+    this.paginationConfig.currentPage = event;
+  }
 }
---
+++ src/app/profile-results.model.ts
@@ -0,0 +1,118 @@
+export const ProfileResultsMock = [
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    }, {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName: 'testLastName',
+        email: 'test@t.com',
+        gender: 'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    },
+    {
+        firstName: 'testFirstName',
+        lastName:'testLastName',
+        email:'test@t.com',
+        gender:'female',
+        department: 'computers',
+        country: 'USA',
+        createdDate: 1618259768854
+    }
+]
